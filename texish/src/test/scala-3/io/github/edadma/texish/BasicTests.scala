f7f74c1aec014811d4b3090d2d644347c0131654
Edward A Maxedon <edadma@gmail.com>
2024-09-22 14:56:48 -0400

    set up testing

--- texish/src/test/scala-3/io/github/edadma/texish/BasicTests.scala
+++ texish/src/test/scala-3/io/github/edadma/texish/BasicTests.scala
@@ -8,3 +8,7 @@ class BasicTests extends AnyFreeSpec with Matchers with Testing:
   "characters" in {
     test("asdf") shouldBe "asdf"
   }
+
+  "simple text" in {
+    test("asdf zxcv") shouldBe "asdf"
+  }
