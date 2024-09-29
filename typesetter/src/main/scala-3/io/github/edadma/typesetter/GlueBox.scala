3bb64378216c885796cc663cbc7d37b3cc5c0115
Edward A Maxedon Sr <edadma@gmail.com>
2024-09-29 13:04:23 -0400

    glue arithmetic

--- typesetter/src/main/scala-3/io/github/edadma/typesetter/GlueBox.scala
+++ typesetter/src/main/scala-3/io/github/edadma/typesetter/GlueBox.scala
@@ -1,37 +1,40 @@
 package io.github.edadma.typesetter
 
-abstract class GlueBox(val naturalWidth: Double, val stretch: Double, val shrink: Double) extends Box:
+abstract class GlueBox(val naturalSize: Double, val stretch: Double, val shrink: Double) extends Box:
   val order: Int
   val name: String
 
   val ascent: Double = 0
   val descent: Double = 0
-  val height: Double = naturalWidth
-  val width: Double = naturalWidth // Initially, it's the natural width
-  val xAdvance: Double = naturalWidth // Same as width initially
+  val height: Double = naturalSize
+  val width: Double = naturalSize // Initially, it's the natural width
+  val xAdvance: Double = naturalSize // Same as width initially
 
   def draw(t: Typesetter, x: Double, y: Double): Unit =
     // No drawing needed for glue
     ()
 
-  override def toString: String = s"${name}Glue(naturalWidth=$naturalWidth, stretch=$stretch, shrink=$shrink)"
+  override def toString: String = s"${name}Glue(naturalSize=$naturalSize, stretch=$stretch, shrink=$shrink)"
 
-class OrdinaryGlueBox(naturalWidth: Double, stretch: Double = 0, shrink: Double = 0)
-    extends GlueBox(naturalWidth, stretch, shrink):
+class OrdinaryGlueBox(naturalSize: Double, stretch: Double = 0, shrink: Double = 0)
+    extends GlueBox(naturalSize, stretch, shrink):
   val order: Int = 0
   val name: String = "Ordinary"
 
-class FilGlueBox(naturalWidth: Double = 0, stretch: Double = 1, shrink: Double = 1)
-    extends GlueBox(naturalWidth, stretch, shrink):
+  def add(space: Double): OrdinaryGlueBox = OrdinaryGlueBox(naturalSize + space, stretch, shrink)
+  def subtract(space: Double): OrdinaryGlueBox = OrdinaryGlueBox(naturalSize - space, stretch, shrink)
+
+class FilGlueBox(naturalSize: Double = 0, stretch: Double = 1, shrink: Double = 1)
+    extends GlueBox(naturalSize, stretch, shrink):
   val order: Int = 1
   val name: String = "Fil"
 
-class FillGlueBox(naturalWidth: Double = 0, stretch: Double = 1, shrink: Double = 1)
-    extends GlueBox(naturalWidth, stretch, shrink):
+class FillGlueBox(naturalSize: Double = 0, stretch: Double = 1, shrink: Double = 1)
+    extends GlueBox(naturalSize, stretch, shrink):
   val order: Int = 2
   val name: String = "Fill"
 
-class FilllGlueBox(naturalWidth: Double = 0, stretch: Double = 1, shrink: Double = 1)
-    extends GlueBox(naturalWidth, stretch, shrink):
+class FilllGlueBox(naturalSize: Double = 0, stretch: Double = 1, shrink: Double = 1)
+    extends GlueBox(naturalSize, stretch, shrink):
   val order: Int = 3
   val name: String = "Filll"
