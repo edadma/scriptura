41775796ae117328b6d4957966420985683b717f
Edward A Maxedon Sr <edadma@gmail.com>
2024-09-29 13:51:57 -0400

    UnitConverter

--- typesetter/src/main/scala-3/io/github/edadma/typesetter/UnitConverter.scala
+++ typesetter/src/main/scala-3/io/github/edadma/typesetter/UnitConverter.scala
@@ -5,7 +5,9 @@ import scala.compiletime.uninitialized
 object UnitConverter:
   var t: Typesetter = uninitialized
 
-  def pointsToPixels(points: Double, dpi: Double): Double = (points / 72.0) * dpi
+  def pointsToPixels(points: Double): Double = (points / 72.0) * t.currentDPI
+
+  def pixelsToPoints(pixels: Double): Double = (pixels / t.currentDPI) * 72.0
 
   def cmToPoints(cm: Double): Double = (cm / 2.54) * 72.0
 
@@ -13,18 +15,21 @@ object UnitConverter:
 
   def mmToPoints(mm: Double): Double = (mm / 25.4) * 72.0
 
+  def picasToPoints(picas: Double): Double = picas * 12.0
+
   def toPoints(value: Double, unit: String): Double = unit match
     case "pt" => value // Points
-    case "pc" => value * 12 // Picas (1 pica = 12 points)
-    case "in" => value * 72 // Inches (1 inch = 72 points)
-    case "cm" => value * 28.3465 // Centimeters (1 cm = 28.3465 points)
-    case "mm" => value * 2.83465 // Millimeters (1 mm = 2.83465 points)
+    case "pc" => picasToPoints(value) // Picas (1 pica = 12 points)
+    case "in" => inchesToPoints(value) // Inches (1 inch = 72 points)
+    case "cm" => cmToPoints(value) // Centimeters (1 cm = 28.3465 points)
+    case "mm" => mmToPoints(value) // Millimeters (1 mm = 2.83465 points)
     case "em" => value * t.currentFontSize // Em units (based on current font size)
     case "ex" => value * t.currentFontXHeight // Ex units (based on current font x-height)
-    case "px" => value * (72.0 / t.currentDPI) // Pixels (dependent on screen DPI)
+    case "px" => pixelsToPoints(value) // Pixels (dependent on screen DPI)
     case _    => throw new IllegalArgumentException(s"Unknown unit: $unit")
 
-  def toPicas(points: Double): String =
+  def toPicas(points: Double): (Double, Double, String) =
     val picas = points / 12
     val remainingPoints = points % 12
-    f"${picas}p${remainingPoints}"
+
+    (picas, points, f"${picas}p${remainingPoints}")
