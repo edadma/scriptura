1547e6c0b3d012153f45774939bf63a3dac89863
Edward A Maxedon Sr <edadma@gmail.com>
2024-09-29 18:56:23 -0400

    supported fonts

--- typesetter/src/main/scala-3/io/github/edadma/typesetter/Typesetter.scala
+++ typesetter/src/main/scala-3/io/github/edadma/typesetter/Typesetter.scala
@@ -2,6 +2,7 @@ package io.github.edadma.typesetter
 
 import scala.collection.mutable
 import scala.compiletime.uninitialized
+import scala.language.postfixOps
 
 abstract class Typesetter:
   type GenericFont
@@ -51,22 +52,155 @@ abstract class Typesetter:
 
   doc.setTypesetter(this)
 
+  loadTypeface(
+    "noto",
+    "fonts/NotoSerif/NotoSerif",
+    "\uFB03\uFB04\uFB01\uFB02",
+    Set(),
+    "Regular",
+    "Bold",
+    "Italic",
+    ("Bold", "Italic"),
+  )
+  loadTypeface(
+    "noto",
+    "fonts/NotoSans/NotoSans",
+    "\uFB03\uFB04\uFB01\uFB02\uFB00",
+    Set("sans"),
+    "Black",
+    ("Black", "Italic"),
+    "Bold",
+    ("Bold", "Italic"),
+    "ExtraBold",
+    ("ExtraBold", "Italic"),
+    "ExtraLight",
+    ("ExtraLight", "Italic"),
+    "Italic",
+    "Light",
+    ("Light", "Italic"),
+    "Medium",
+    ("Medium", "Italic"),
+    "Regular",
+    "SemiBold",
+    ("SemiBold", "Italic"),
+    "Thin",
+    ("Thin", "Italic"),
+  )
+  loadTypeface(
+    "gentium",
+    "fonts/GentiumPlus-6.200/GentiumPlus",
+    "\uFB03\uFB04\uFB01\uFB02\uFB00",
+    Set(),
+    "Regular",
+    "Bold",
+    "Italic",
+    ("Bold", "Italic"),
+  )
+  loadTypeface(
+    "gentiumbook",
+    "fonts/GentiumBookPlus/GentiumBookPlus",
+    "\uFB03\uFB04\uFB01\uFB02\uFB00",
+    Set(),
+    "Regular",
+    "Bold",
+    "Italic",
+    ("Bold", "Italic"),
+  )
+  overrideBaseline("gentium", 0.8)
+  loadTypeface("pt", "fonts/PTSansNarrow/PTSansNarrow", "\uFB01\uFB02", Set(), "Regular", "Bold")
+  loadTypeface(
+    "mono",
+    "fonts/JetBrainsMono/static/JetBrainsMono",
+    "",
+    Set(),
+    "Bold",
+    ("Bold", "Italic"),
+    "ExtraBold",
+    ("ExtraBold", "Italic"),
+    "ExtraLight",
+    ("ExtraLight", "Italic"),
+    "Italic",
+    "Light",
+    ("Light", "Italic"),
+    "Medium",
+    ("Medium", "Italic"),
+    "Regular",
+    "SemiBold",
+    ("SemiBold", "Italic"),
+    "Thin",
+    ("Thin", "Italic"),
+  )
+  loadTypeface(
+    "alegreya",
+    "fonts/Alegreya/static/Alegreya",
+    "\uFB01\uFB02",
+    Set(),
+    "Black",
+    ("Black", "Italic"),
+    "Bold",
+    ("Bold", "Italic"),
+    "ExtraBold",
+    ("ExtraBold", "Italic"),
+    "Italic",
+    "Medium",
+    ("Medium", "Italic"),
+    "Regular",
+    "SemiBold",
+    ("SemiBold", "Italic"),
+  )
+  loadTypeface(
+    "alegreya",
+    "fonts/AlegreyaSC/AlegreyaSC",
+    "\uFB01\uFB02",
+    Set("smallcaps"),
+    "Black",
+    ("Black", "Italic"),
+    "Bold",
+    ("Bold", "Italic"),
+    "ExtraBold",
+    ("ExtraBold", "Italic"),
+    "Italic",
+    "Medium",
+    ("Medium", "Italic"),
+    "Regular",
+  )
+
   def loadFont(typeface: String, path: String, ligatures: Set[String], styleSet: Set[String]): Unit =
-    val face: GenericFont = loadFont(path) /*fontFaceCreateForFTFace(
-      freetype
-        .newFace(path, 0)
-        .getOrElse(sys.error(s"error loading face: $path"))
-        .faceptr,
-      0,
-    )*/
+    val font = loadFont(path)
 
     typefaces get typeface match
-      case None => typefaces(typeface) = Typeface(mutable.HashMap(styleSet -> face), None, ligatures)
+      case None => typefaces(typeface) = Typeface(mutable.HashMap(styleSet -> font), None, ligatures)
       case Some(Typeface(fonts, _, ligatures)) =>
         if fonts contains styleSet then
           sys.error(s"font for typeface '$typeface' with style '${styleSet.mkString(", ")}' has already been loaded")
-        else fonts(styleSet) = face
+        else fonts(styleSet) = font
   end loadFont
+
+  def loadTypeface(
+      typeface: String,
+      basepath: String,
+      ligatures: String,
+      every: Set[String],
+      styles: (Product | String)*,
+  ): Unit =
+    for style <- styles do
+      val (styleName, styleSet) =
+        style match
+          case s: String  => (s, if s.toLowerCase == "regular" then Set.empty else Set(s.toLowerCase))
+          case p: Product => (p.productIterator.mkString, p.productIterator.map(_.toString.toLowerCase).toSet)
+
+      loadFont(
+        typeface,
+        s"$basepath-$styleName.ttf",
+        ligatures map (_.toString) toSet,
+        styleSet ++ every.map(_.toLowerCase),
+      )
+  end loadTypeface
+
+  def overrideBaseline(typeface: String, baseline: Double): Unit =
+    typefaces get typeface match
+      case None                                => sys.error(s"typeface '$typeface' not found")
+      case Some(Typeface(fonts, _, ligatures)) => typefaces(typeface) = Typeface(fonts, Some(baseline), ligatures)
 
 end Typesetter
 
