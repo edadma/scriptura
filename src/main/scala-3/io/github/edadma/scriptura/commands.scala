bbf1503a3bd46028842d504bca467bf3c7cdc31e
Edward A Maxedon <edadma@gmail.com>
2024-10-13 17:57:19 -0400

    commands

--- src/main/scala-3/io/github/edadma/scriptura/commands.scala
+++ src/main/scala-3/io/github/edadma/scriptura/commands.scala
@@ -1,6 +1,6 @@
 package io.github.edadma.scriptura
 
-import io.github.edadma.typesetter.Typesetter
+import io.github.edadma.typesetter.{FilGlue, Typesetter}
 import io.github.edadma.texish.{AST, Active, Command, Parser, Renderer, problem}
 import io.github.edadma.char_reader.CharReader
 
@@ -57,4 +57,40 @@ val commands =
               context.asInstanceOf[Typesetter].noitalic()
           case List(a) => problem(pos, s"expected arguments <text>: $a")
           case _       => problem(pos, "expected arguments <text>"),
+    new Command("smallcaps", 1, false):
+      def apply(
+          pos: CharReader,
+          renderer: Renderer,
+          args: List[Any],
+          optional: Map[String, Any],
+          context: Any,
+      ): Any =
+        args match
+          case List(a: AST) =>
+            () =>
+              context.asInstanceOf[Typesetter].smallcaps()
+              renderer.render(a)
+              context.asInstanceOf[Typesetter].nosmallcaps()
+          case List(a) => problem(pos, s"expected arguments <text>: $a")
+          case _       => problem(pos, "expected arguments <text>")
+    ,
+    new Command("hfil", 0, false):
+      def apply(
+          pos: CharReader,
+          renderer: Renderer,
+          args: List[Any],
+          optional: Map[String, Any],
+          context: Any,
+      ): Any =
+        FilGlue
+    ,
+    new Command("vfil", 0, false):
+      def apply(
+          pos: CharReader,
+          renderer: Renderer,
+          args: List[Any],
+          optional: Map[String, Any],
+          context: Any,
+      ): Any =
+        FilGlue,
   )
