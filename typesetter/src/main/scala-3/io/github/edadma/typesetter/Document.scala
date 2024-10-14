5b3724adfef1d99c6fe11047ea8bc6fafcd179c9
Edward A Maxedon <edadma@gmail.com>
2024-10-13 22:51:46 -0400

    Document

--- typesetter/src/main/scala-3/io/github/edadma/typesetter/Document.scala
+++ typesetter/src/main/scala-3/io/github/edadma/typesetter/Document.scala
@@ -1,13 +1,19 @@
 package io.github.edadma.typesetter
 
+import scala.collection.mutable.ArrayBuffer
 import scala.compiletime.uninitialized
 
 abstract class Document extends Mode:
   private[typesetter] var ts: Typesetter = uninitialized
+  val pages = new ArrayBuffer[Any]
 
   def t: Typesetter = ts
 
   def init(): Unit
+
+  def page(b: Box): Box
+
+  def add(box: Box): Unit = pages += t.render(page(box), t.getNumber("hsize"), t.getNumber("vsize"))
 
   override def done(): Unit = pop
 
--- typesetter/src/main/scala-3/io/github/edadma/typesetter/SimpleDocument.scala
+++ typesetter/src/main/scala-3/io/github/edadma/typesetter/SimpleDocument.scala
@@ -1,14 +1,6 @@
 package io.github.edadma.typesetter
 
-import scala.collection.mutable.ArrayBuffer
-
 class SimpleDocument extends Document:
-  val pages = new ArrayBuffer[Any]
-
   def init(): Unit = ()
 
-  def nonEmpty: Boolean = ???
-
-  override def done(): Unit = pop
-
-  def add(box: Box): Unit = pages += t.render(box, t.getNumber("hsize"), t.getNumber("vsize"))
+  def page(b: Box): Box = b
