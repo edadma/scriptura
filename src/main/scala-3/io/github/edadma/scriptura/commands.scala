f65229476e974a70d23da66f8fdbb2610cf89951
Edward A Maxedon Sr <edadma@gmail.com>
2024-10-20 22:12:32 -0400

    vskip

--- src/main/scala-3/io/github/edadma/scriptura/commands.scala
+++ src/main/scala-3/io/github/edadma/scriptura/commands.scala
@@ -1,6 +1,6 @@
 package io.github.edadma.scriptura
 
-import io.github.edadma.typesetter.{FilGlue, FillGlue, ImageBox, InfGlue, RuleBox, Typesetter}
+import io.github.edadma.typesetter.{FilGlue, FillGlue, ImageBox, InfGlue, RuleBox, Typesetter, Glue}
 import io.github.edadma.texish.{AST, Active, Command, Parser, Renderer, problem}
 import io.github.edadma.char_reader.CharReader
 
@@ -75,6 +75,17 @@ val commands =
           case a         => problem(pos, s"expected a path: $a")
         }
     ,
+    new Command("vskip", 1, true):
+      def apply(
+          pos: CharReader,
+          renderer: Renderer,
+          args: List[Any],
+          optional: Map[String, Any],
+          context: Any,
+      ): Any =
+        args match
+          case List(d: Number) => Glue(d, 0, 0, 0)
+          case _               => problem(pos, "expected arguments <dimen>"),
     new Command("hbox", 1, false):
       def apply(
           pos: CharReader,
@@ -95,7 +106,8 @@ val commands =
             context.asInstanceOf[Typesetter].pop()
             r
           case List(a) => problem(pos, s"expected arguments <text>: $a")
-          case _       => problem(pos, "expected arguments <text>"),
+          case _       => problem(pos, "expected arguments <text>")
+    ,
     new Command("bold", 1, false):
       def apply(
           pos: CharReader,
