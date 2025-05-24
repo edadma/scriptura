317d9cbce2059de003705aedd74f5b24368ec46c
Edward A Maxedon <edadma@gmail.com>
2025-05-24 15:29:47 -0400

    add tract-en-1.script

--- native/src/main/scala-3/io/github/edadma/scriptura/app.scala
+++ native/src/main/scala-3/io/github/edadma/scriptura/app.scala
@@ -2,7 +2,7 @@ package io.github.edadma.scriptura
 
 import io.github.edadma.char_reader.CharReader
 import io.github.edadma.texish.{Active, Command, Parser, Renderer}
-import io.github.edadma.typesetter.{CairoPDFTypesetter, Typesetter, ZFoldedDocument}
+import io.github.edadma.typesetter.{CairoPDFTypesetter, Typesetter}
 import pprint.*
 
 import java.io.FileOutputStream
@@ -40,9 +40,7 @@ def app(args: Config): Unit =
             paper match
               case "a4"     => // Paper.A4
               case "letter" => // Paper.LETTER
-          new CairoPDFTypesetter(output.toString) {
-            setDocument(new ZFoldedDocument)
-          }
+          new CairoPDFTypesetter(output.toString)
 
 //        case Config(_, _, "png", _, resolution, size, _, _) =>
 //          val (width, height) =
